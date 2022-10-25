1629f25a010a64207e3ca7364afda9b304113028
Victor Guillén <victorguillofficial@gmail.com>
2022-10-25 13:56:14 +0200

    ADD JS logic with css

---
+++ js/gamesLogic.js
@@ -0,0 +1,32 @@
+const gamesContainer = document.getElementById("gamesContainer");
+
+const game1 = document.getElementById("game1");
+const game2 = document.getElementById("game2");
+const game3 = document.getElementById("game3");
+
+let elementsHover = {};
+
+onmouseover = (e) => {
+  changeBackground(e.target.id);
+};
+
+function changeBackground(id) {
+  let color;
+
+  switch (id) {
+    case "game1":
+      color = "red";
+      break;
+    case "game2":
+      color = "green";
+      break;
+    case "game3":
+      color = "blue";
+      break;
+    case "gamesContainer":
+      color = "white";
+      break;
+  }
+
+  gamesContainer.style.backgroundColor = color;
+}
