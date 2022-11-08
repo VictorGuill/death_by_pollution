83645796e47dcd3043fa2fb454504265995b4847
Maria Garriga <mariagarriga10@gmail.com>
2022-11-08 09:48:28 +0100

    Random trees and mountains
    faltaria evitar el start i el end
    juntar codi perquè estem duplicant

--- js/game2.js
+++ js/game2.js
@@ -1,3 +1,48 @@
+function rand(n) {
+    // creamos un numero al azar entre la cantidad de imágenes
+    return (Math.floor(Math.random() * n + 1));
+}
+//guardas las imagenes en el array
+var rand_img = new Array();
+rand_img[0] = "../media/br.jpg";
+rand_img[1] = "../media/lb.jpg";
+rand_img[2] = "../media/lr.jpg";
+rand_img[3] = "../media/lt.jpg";
+rand_img[4] = "../media/tb.jpg";
+rand_img[5] = "../media/tr.jpg";
+
+function randomPiece() {
+    document.getElementById("piece").src = rand_img[rand(rand_img.length) - 1];
+    document.getElementById("piece2").src = rand_img[rand(rand_img.length) - 1];
+    document.getElementById("piece3").src = rand_img[rand(rand_img.length) - 1];
+}
+
+const getRandom = (min, max) => Math.floor(Math.random()*(max-min+1)+min);
+
+const square = document.getElementById("tree")
+square.style.gridColumnStart = getRandom(0, 14); // 👈🏼 Horizontally
+square.style.gridRowStart    = getRandom(0, 14); // 👈🏼 Vertically
+
+const square2 = document.getElementById("tree2")
+square2.style.gridColumnStart = getRandom(0, 14); // 👈🏼 Horizontally
+square2.style.gridRowStart    = getRandom(0, 14); // 👈🏼 Vertically
+
+const square3 = document.getElementById("tree3")
+square3.style.gridColumnStart = getRandom(0, 14); // 👈🏼 Horizontally
+square3.style.gridRowStart    = getRandom(0, 14); // 👈🏼 Vertically
+
+const square4 = document.getElementById("tree4")
+square4.style.gridColumnStart = getRandom(0, 14); // 👈🏼 Horizontally
+square4.style.gridRowStart    = getRandom(0, 14); // 👈🏼 Vertically
+
+const mount = document.getElementById("mountain")
+mount.style.gridColumnStart = getRandom(0, 14); // 👈🏼 Horizontally
+mount.style.gridRowStart    = getRandom(0, 14); // 👈🏼 Vertically
+
+const mount2 = document.getElementById("mountain2")
+mount2.style.gridColumnStart = getRandom(0, 14); // 👈🏼 Horizontally
+mount2.style.gridRowStart    = getRandom(0, 14); // 👈🏼 Vertically
+
 let currentDroppable = null;
 
 piece.onmousedown = function (event) {
@@ -158,108 +203,5 @@ piece3.onmousedown = function (event) {
 };
 
 piece3.ondragstart = function () {
-    return false;
-};
-
-
-piece4.onmousedown = function (event) {
-
-    let shiftX = event.clientX - piece4.getBoundingClientRect().left;
-    let shiftY = event.clientY - piece4.getBoundingClientRect().top;
-
-    piece4.style.position = 'absolute';
-    piece4.style.zIndex = 1000;
-    document.body.append(piece);
-
-    moveAt(event.pageX, event.pageY);
-
-    function moveAt(pageX, pageY) {
-        piece4.style.left = pageX - shiftX + 'px';
-        piece4.style.top = pageY - shiftY + 'px';
-    }
-
-    function onMouseMove(event) {
-        moveAt(event.pageX, event.pageY);
-
-        piece4.hidden = true;
-        let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
-        piece4.hidden = false;
-
-        if (!elemBelow) return;
-
-        let droppableBelow = elemBelow.closest('.droppable');
-        if (currentDroppable != droppableBelow) {
-            if (currentDroppable) { // null when we were not over a droppable before this event
-                leaveDroppable(currentDroppable);
-            }
-            currentDroppable = droppableBelow;
-            if (currentDroppable) { // null if we're not coming over a droppable now
-                // (maybe just left the droppable)
-                enterDroppable(currentDroppable);
-            }
-        }
-    }
-
-    document.addEventListener('mousemove', onMouseMove);
-
-    piece4.onmouseup = function () {
-        document.removeEventListener('mousemove', onMouseMove);
-        piece4.onmouseup = null;
-    };
-
-};
-
-piece4.ondragstart = function () {
-    return false;
-};
-
-piece5.onmousedown = function (event) {
-
-    let shiftX = event.clientX - piece5.getBoundingClientRect().left;
-    let shiftY = event.clientY - piece5.getBoundingClientRect().top;
-
-    piece5.style.position = 'absolute';
-    piece5.style.zIndex = 1000;
-    document.body.append(piece);
-
-    moveAt(event.pageX, event.pageY);
-
-    function moveAt(pageX, pageY) {
-        piece5.style.left = pageX - shiftX + 'px';
-        piece5.style.top = pageY - shiftY + 'px';
-    }
-
-    function onMouseMove(event) {
-        moveAt(event.pageX, event.pageY);
-
-        piece5.hidden = true;
-        let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
-        piece5.hidden = false;
-
-        if (!elemBelow) return;
-
-        let droppableBelow = elemBelow.closest('.droppable');
-        if (currentDroppable != droppableBelow) {
-            if (currentDroppable) { // null when we were not over a droppable before this event
-                leaveDroppable(currentDroppable);
-            }
-            currentDroppable = droppableBelow;
-            if (currentDroppable) { // null if we're not coming over a droppable now
-                // (maybe just left the droppable)
-                enterDroppable(currentDroppable);
-            }
-        }
-    }
-
-    document.addEventListener('mousemove', onMouseMove);
-
-    piece5.onmouseup = function () {
-        document.removeEventListener('mousemove', onMouseMove);
-        piece5.onmouseup = null;
-    };
-
-};
-
-piece5.ondragstart = function () {
     return false;
 };
