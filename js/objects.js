aa80aecada970e132c3d5b1947092ae5a6cffc53
Maria-examen <mariagarriga10@gmail.com>
2022-11-18 13:41:54 +0100

    ultima versio sobreposition

--- js/objects.js
+++ js/objects.js
@@ -1,5 +1,5 @@
-const numMounts = 90;
-const numTrees = 100;
+const numMounts = 6;
+const numTrees = 50;
 const size = 14;
 
 const getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
@@ -30,66 +30,40 @@ tree.forEach(
         div.className = "tree";
         div.innerHTML = '<img src="../media/game2/thetree.png" width="60px" height="60px"/>';
         document.querySelector('#map').appendChild(div);
-        div.style.color = 'white';
-        div.style.width = '60px';
-        div.style.height = '60px';
-        div.style.borderRadius = '30px';
-        div.style.position = 'relative';
+        
         randomPosition(div, false);
     }
 );
 
 //Function for the random position of the objects.
 function randomPosition(obj, isMount) {
-    let startEl = document.querySelector('#start');
-    let startPosition = getComputedStyle(startEl);
-    let endEl = document.querySelector('#end');
-    let endPosition = getComputedStyle(endEl);
-
-    do {
-
+    let x = 0;
+    let full = false;
+    do{
         if (isMount) {
             obj.style.gridColumnStart = getRandom(1, size - 1); //Column position
-            obj.style.gridRowStart = getRandom(1, size); //Row position
         } else {
             obj.style.gridColumnStart = getRandom(1, size); //Column position
-            obj.style.gridRowStart = getRandom(1, size); //Row position
         }
+        obj.style.gridRowStart = getRandom(1, size); //Row position
 
-        if (fullPosition(obj.style.gridColumnStart, obj.style.gridRowStart)) {
-            if (isMount) {
-                obj.style.gridColumnStart = getRandom(1, size - 1); //Column position
-                obj.style.gridRowStart = getRandom(1, size); //Row position
-            } else {
-                obj.style.gridColumnStart = getRandom(1, size); //Column position
-                obj.style.gridRowStart = getRandom(1, size); //Row position
-            }
-        }
-
-    } while ((obj.style.gridColumnStart == startPosition.gridColumnStart
-        && obj.style.gridRowStart == startPosition.gridRowStart)
-        || (obj.style.gridColumnStart == endPosition.gridColumnStart
-            && obj.style.gridRowStart == endPosition.gridRowStart));
-
+        full = fullPosition(obj.style.gridColumnStart, obj.style.gridRowStart);
+        x++;
+    } while (full && x < 10);
 }
 
-/* FUNCTION TO KNOW THE FULL POSITIONS.
- * 
- */
+//FUNCTION TO KNOW THE FULL POSITIONS.
 function fullPosition(col, row) {
-    let mountElems = document.querySelectorAll('.mount');
-    let i = 0;
-    let fullPosition = false;
+    let divElems = document.querySelectorAll('#map div');
+    let i = 0, fullPosition = false;
 
-    while (i < mountElems.length && !fullPosition) {
-        let mountStyle = getComputedStyle(mountElems[i]);
-        if (col == mountStyle.gridColumnStart && row == mountStyle.gridRowStart) {
+    while (i < divElems.length-1 && !fullPosition) {
+        let divStyle = getComputedStyle(divElems[i]);
+        if (col == divStyle.gridColumnStart && row == divStyle.gridRowStart) {
             fullPosition = true;
-        }
-        else {
+        } else {
             i++;
         }
     }
-
     return fullPosition;
 }
