34ea946a58ee63a02ef8a1b97e51adc9ea34b4b2
Maria-examen <mariagarriga10@gmail.com>
2022-11-23 10:26:34 +0100

    add class and id to the img

--- js/game2/pieces.js
+++ js/game2/pieces.js
@@ -1,16 +1,16 @@
 export default class Piece {
-    constructor(id) {
-        this.id = id;
-
+    constructor() {
         this.doc = document.createElement("div");
         this.doc.setAttribute("draggable", true);
         this.id = "piece";
     }
 
 
-    addPiece() {
+    addPiece(id) {
         const imgPiece = this.randomPiece();
-        this.doc.innerHTML = `<img src="` + imgPiece + `" width="60px" height="60px"/>`;
+        let srcPiece = imgPiece.src;
+        let classPiece = imgPiece.class;
+        this.doc.innerHTML = `<img id ="p`+ id +`" class ="img`+ classPiece +`" src="` + srcPiece + `" width="60px" height="60px"/>`;
         this.doc.id = this.id;
 
         //items picker as the father of the pieces
@@ -19,21 +19,26 @@ export default class Piece {
     }
 
     randomPiece() {
-        var rand_img = new Array();
-        rand_img[0] = "../media/game2/tracks/br.jpg";
-        rand_img[1] = "../media/game2/tracks/lb.jpg";
-        rand_img[2] = "../media/game2/tracks/lr.jpg";
-        rand_img[3] = "../media/game2/tracks/lt.jpg";
-        rand_img[4] = "../media/game2/tracks/tb.jpg";
-        rand_img[5] = "../media/game2/tracks/tr.jpg";
+        //image array route
+        let rand_img = new Array();
+        rand_img[0] = "../media/game2/tracks/lb.png";
+        rand_img[1] = "../media/game2/tracks/tb.png";
+        rand_img[2] = "../media/game2/tracks/lr.png";
+        rand_img[3] = "../media/game2/tracks/br.png";
+        rand_img[4] = "../media/game2/tracks/lt.png";
+        rand_img[5] = "../media/game2/tracks/tr.png";
 
-        function rand(n) {
-            // creamos un numero al azar entre la cantidad de imágenes
-            return (Math.floor(Math.random() * n + 1));
-        }
+        let random = this.rand(rand_img.length);
+        this.src = rand_img[random];
         
-        this.src = rand_img[rand(rand_img.length) - 1];
-        return this.src;
+        return {
+            src: this.src,
+            class: random
+          };
     }
-
+    
+    //creating a random num betweeen 0 and the number of img 
+    rand(max) {
+        return (Math.floor(Math.random() * max));
+    }
 }
