25bd33e07df5c30495eb43da5ce6cc1d3bcfa9f7
Victor Guillén Alcaraz <victorguillofficial@gmail.com>
2022-11-24 23:10:39 +0100

    rework landing
    Falta el efecto after a la navbar y rellenar el landing

---
+++ js/landing.js
@@ -0,0 +1,35 @@
+// get user browser window height
+let viewportHeight = Math.max(
+  document.documentElement.clientHeight || 0,
+  window.innerHeight || 0
+);
+
+// pages container and navbar
+const container = document.getElementById("scroll-container");
+const nabvar = document.getElementById("landing_nav");
+const goStartBtn = document.getElementById("goStartBtn");
+
+// set navbar background color
+container.addEventListener("scroll", (e) => {
+  if (container.scrollTop >= viewportHeight - viewportHeight / 3) {
+    nabvar.style.backgroundColor = " var(--DARK)";
+  } else {
+    nabvar.style.backgroundColor = "rgba(0, 0, 0, 0)";
+  }
+
+  if (container.scrollTop >= viewportHeight / 3) {
+    // goStartBtn.style.display = "inherit";
+    goStartBtn.style.opacity = "1";
+  } else {
+    // goStartBtn.style.display = "none";
+    goStartBtn.style.opacity = "0";
+  }
+});
+
+// set new browser window height value
+window.addEventListener("resize", function (e) {
+  viewportHeight = Math.max(
+    document.documentElement.clientHeight || 0,
+    window.innerHeight || 0
+  );
+});
