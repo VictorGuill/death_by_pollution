50b7c7466bb3f39859de06a6a16a20b3434e0d24
Victor Guillén Alcaraz <victorguillofficial@gmail.com>
2022-12-01 17:14:59 +0100

    FIX NABVAR MISSING LINKS

--- pages/gamesMenu.php
+++ pages/gamesMenu.php
@@ -6,6 +6,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Games menu</title>
+  <link rel="icon" type="image/x-icon" href="/death_by_pollution/media/icons/favicon32.png">
+  <link rel="stylesheet" href="/death_by_pollution/tercers/boostrap5/bootstrap.min.css" />
+  <script src="/death_by_pollution/tercers/boostrap5/bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" href="../style/colores.css" />
   <link rel="stylesheet" href="../style/landing.css" />
   <link rel="stylesheet" href="../style/gamesMenu.css" />
