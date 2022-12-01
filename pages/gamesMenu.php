eb28e0312f25c5421ed0d4a877a5eabcb2fc9269
Maria <mariagarriga10@gmail.com>
2022-12-01 16:56:44 +0100

    add game2 img
    screenshoot and name

--- pages/gamesMenu.php
+++ pages/gamesMenu.php
@@ -6,9 +6,6 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Games menu</title>
-  <link rel="icon" type="image/x-icon" href="/death_by_pollution/media/icons/favicon32.png">
-  <link rel="stylesheet" href="/death_by_pollution/tercers/boostrap5/bootstrap.min.css" />
-  <script src="/death_by_pollution/tercers/boostrap5/bootstrap.bundle.min.js"></script>
   <link rel="stylesheet" href="../style/colores.css" />
   <link rel="stylesheet" href="../style/landing.css" />
   <link rel="stylesheet" href="../style/gamesMenu.css" />
@@ -44,7 +41,7 @@
         <p class="title_game">SUPER SCUBA</p>
       </div>
       <div id="game2" class="game">
-        <p class="title_game">JUEGO 2</p>
+        <p class="title_game">TRAIN TRACK</p>
       </div>
       <div id="game3" class="game">
         <p class="title_game">JUEGO 3</p>
