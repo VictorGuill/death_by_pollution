cb9a7d32c5e3dc2d994bf864008f4ad26a12a89b
GUILLEM TURRÓ <gturro97@gmail.com>
2022-12-17 22:37:21 +0100

    removed cross close button from userEdit modal

--- pages/adminPanel.php
+++ pages/adminPanel.php
@@ -135,8 +135,6 @@ if (!isset($_SESSION)) {
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title">Editar usuario</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-          </button>
         </div>
         <div class="modal-body">
           <form method="POST" action='../php_controllers/controller.php' enctype="multipart/form-data">
